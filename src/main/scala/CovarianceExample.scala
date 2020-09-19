c10a64fa2ac40452556da4d7b0339c36345159c2
codelyf169 <pranaykundu16@gmail.com>
2020-09-20 03:05:31 +0530

    Covariance Example - FP scala 101

---
+++ src/main/scala/CovarianceExample.scala
@@ -0,0 +1,24 @@
+object DemoApp {
+  def main(args: Array[String]): Unit = {
+    val maleCharacters: List[MaleCharacter] = List(MaleCharacter("Heisenberg"), MaleCharacter("Jesse Pinkman"), MaleCharacter("Gus Fring"))
+    val femaleCharacters: List[FemaleCharacter] = List(FemaleCharacter("Skyler White"), FemaleCharacter("Marie Schrader"))
+
+    println("First printing Male characters!")
+    sayMyName(maleCharacters)
+
+    println("Second printing Female characters!")
+    sayMyName(femaleCharacters)
+  }
+
+  def sayMyName(characters: List[BreakingBadCharacter]): Unit = characters.foreach { character =>
+    println(s"My Name is ${character.name}")
+  }
+}
+
+abstract class BreakingBadCharacter {
+  def name: String
+}
+
+case class MaleCharacter(name: String) extends BreakingBadCharacter
+
+case class FemaleCharacter(name: String) extends BreakingBadCharacter
